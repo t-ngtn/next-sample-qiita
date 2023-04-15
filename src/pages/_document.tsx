837179e474451d7183d57dc3d7c6811b796eafd5
t-ngtn <obutaiei@gmail.com>
2023-04-16 01:16:06 +0900

    update: move Header -> _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,16 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ja">
+      <Head>
+        <title>Qiita Sample</title>
+        <meta
+          name="description"
+          content="Sample project using Qiita's API created with Next.js"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
