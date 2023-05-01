b9c82c24aeb568d799908aa5e947a3a9377e7ddd
t-ngtn <obutaiei@gmail.com>
2023-05-01 10:15:38 +0900

    update api.tsx

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,21 +1,65 @@
 import axios from 'axios';
 
-export const fetchPosts = async () => {
-  try {
-    const response = await axios.get('https://qiita.com/api/v2/items', {
+export type QiitaPostData = {
+  id: string;
+  title: string;
+  user_id: string;
+  username: string;
+  created_at: string;
+};
+
+export const createQiitaPostData = (
+  id: string,
+  title: string,
+  user_id: string,
+  username: string,
+  created_at: string
+): QiitaPostData => {
+  return {
+    id,
+    title,
+    user_id,
+    username,
+    created_at,
+  };
+};
+
+const ACCESS_TOKEN = '';
+
+export const fetchPosts = async (setRows: Function) => {
+  await axios
+    .get('https://qiita.com/api/v2/items', {
+      headers: {
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+      },
       params: { page: 1, per_page: 20 },
+    })
+    .then((response) => {
+      return JSON.parse(JSON.stringify(response.data));
+    })
+    .then((res) => {
+      const posts: QiitaPostData[] = res.map((data: any) => {
+        return createQiitaPostData(
+          data.id,
+          data.title,
+          data.user.id,
+          data.user.name,
+          data.created_at
+        );
+      });
+      setRows(posts);
+    })
+    .catch((error) => {
+      console.log(error);
     });
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
 };
 
 export const fetchPost = async (item_id: string) => {
   try {
-    const response = await axios.get('https://qiita.com/api/v2/items' + item_id);
-    return response.data
+    const response = await axios.get(
+      'https://qiita.com/api/v2/items' + item_id
+    );
+    return response.data;
   } catch (error) {
     console.error(error);
     throw error;
