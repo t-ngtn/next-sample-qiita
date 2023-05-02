294871d858b984058f8d1678c33ffaa88ca71df8
t-ngtn <obutaiei@gmail.com>
2023-05-02 10:37:08 +0900

    update color

--- src/components/molecules/TopBar.tsx
+++ src/components/molecules/TopBar.tsx
@@ -1,13 +1,13 @@
-import { AppBar, IconButton, Toolbar, Typography} from "@mui/material";
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
 
 export default function TopBar() {
-    return (
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6">
-                    Qiita Sample App
-                </Typography>
-            </Toolbar>
-        </AppBar>
-    )
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" color="text.secondary">
+          Qiita Sample App
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,19 +3,23 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-// const theme = createTheme({
-//   palette: {
-//     primary: {
-//       main: '#4caf50',
-//     },
-//   },
-// });
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4caf50',
+    },
+    text: {
+      primary: '#000000',
+      secondary: '#ffffffff',
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Qiita Sample</title>
+        <title>Qiita Sample App</title>
         <meta
           name="description"
           content="Sample project using Qiita's API created with Next.js"
@@ -23,7 +27,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
