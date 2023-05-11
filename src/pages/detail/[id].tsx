65988f10a5ec16759425d50c7c9801e8ebcfa4ce
t-ngtn <obutaiei@gmail.com>
2023-05-12 00:43:04 +0900

    update [id].tsx rendered_bodyを使うように修正

--- src/pages/detail/[id].tsx
+++ src/pages/detail/[id].tsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { apiTokenState } from '@/utils/state';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 export default function ItemDetal() {
   const router = useRouter();
@@ -15,7 +15,7 @@ export default function ItemDetal() {
 
   const handleGetById = () => {
     if (typeof id === 'string') {
-      fetchItem(setItem, id, '');
+      fetchItem(setItem, id, token);
     }
   };
 
@@ -26,15 +26,18 @@ export default function ItemDetal() {
   return (
     <div className="container">
       <TopBar token={token} setToken={setToken} open={open} setOpen={setOpen} />
-      <Box
-        m={2}
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-      >
-        {' '}
+      <Box m={2}>
+        <Typography variant="h4" component="h1">
+          {item?.title}
+        </Typography>
       </Box>
-      <div>{item?.rendered_body}</div>
+      <Box m={2}>
+        {item?.rendered_body ? (
+          <div dangerouslySetInnerHTML={{ __html: item.rendered_body }} />
+        ) : (
+          <p>Loading...</p>
+        )}
+      </Box>
     </div>
   );
 }
